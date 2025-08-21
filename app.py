d307bb5af3f2d1be4542f5f260ededa6f7cb04e2
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-08-20 21:27:27 -0300

    Fix schema, add missing crud routes and methods

--- app.py
+++ app.py
@@ -13,15 +13,67 @@ app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
 
 mysql = MySQL(app)
 
-# Routing
+# Select
 @app.route('/')
 def index():
-    query = mysql.connection.cursor()
-    query.execute("SELECT * FROM users")
-    data = query.fetchall()
-    query.close()
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM users")
+    data = cursor.fetchall()
+    cursor.close()
     return render_template('index.html', users=data)
 
+# Create
+@app.route('/create', methods=['POST'])
+def create_user():
+    name = request.form['name']
+    age = request.form['age']
+    email = request.form['email']
+    
+    cursor = mysql.connection.cursor()
+    cursor.execute("INSERT INTO users (patient_name, age, email) VALUES (%s, %s, %s)", (name, age, email))
+    mysql.connection.commit()
+    cursor.close()
+    
+    return redirect('/')
+
+# Delete
+@app.route('/delete/<int:user_id>', methods=['POST'])
+def delete_user(user_id):
+    cursor = mysql.connection.cursor()
+    cursor.execute("DELETE FROM users WHERE id = %s", (user_id,))
+    mysql.connection.commit()
+    cursor.close()
+    return redirect('/')
+
+# Edit
+@app.route('/edit/<int:user_id>')
+def edit_user(user_id):
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE id = %s", (user_id,))
+    user = cursor.fetchone()
+    cursor.close()
+    if user:
+        return render_template('edit.html', user=user)
+    else:
+        return "User not found", 404
+
+# Update
+@app.route('/update/<int:user_id>', methods=['POST'])
+def update_user(user_id):
+    name = request.form['name']
+    age = request.form['age']
+    email = request.form['email']
+
+    cursor = mysql.connection.cursor()
+    cursor.execute("""
+        UPDATE users 
+        SET patient_name=%s, age=%s, email=%s 
+        WHERE id=%s
+    """, (name, age, email, user_id))
+    mysql.connection.commit()
+    cursor.close()
+
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database/main_schema.sql
+++ database/main_schema.sql
@@ -5,9 +5,9 @@ CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     patient_name VARCHAR(100) NOT NULL,
     age INT,
-    appointment_date DATE
-)
-INSERT INTO users (patient_name, age, appointment_date)
-VALUES ('James Morgan', 44, '2025-09-14');
-INSERT INTO users (patient_name, age, appointment_date)
-VALUES ('Carl Weather', 66, '2025-10-12');
+    email VARCHAR(100) NOT NULL
+);
+INSERT INTO users (patient_name, age, email)
+VALUES ('James Morgan', 44, 'jamesmorgan@gmail.com');
+INSERT INTO users (patient_name, age, email)
+VALUES ('Carl Weather', 66, 'carlweather@gmail.com');
